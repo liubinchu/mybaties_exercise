41719bc8da3323509199cfe807f9d850c9f1f50b
liubi <liubinchu@outlook.com>
2020-09-03 15:26:55 +0800

    9.2

--- src/test/java/com/sample/AccountTest.java
+++ src/test/java/com/sample/AccountTest.java
@@ -1,8 +1,16 @@
 package com.sample;
 
 import com.sample.dao.IAccountDao;
+import com.sample.dao.IUserDao;
 import com.sample.domain.AccountUser;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -12,6 +20,24 @@ import java.util.*;
 public class AccountTest {
 
     private IAccountDao accountDao;
+    private InputStream in;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+
+    @Before
+    public void setUp() throws Exception {
+// 1.读取配置文件
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+// 2.创建构建者对象
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+// 3.创建SqlSession工厂对象
+        factory = builder.build(in);
+// 4.创建SqlSession对象
+        session = factory.openSession();
+        // 5.创建Dao的代理对象
+        accountDao = session.getMapper(IAccountDao.class);
+    }
+
     @Test
     public void testFindAll() {
 //6.执行操作
--- src/test/java/com/sample/RoleTest.java
+++ src/test/java/com/sample/RoleTest.java
@@ -1,9 +1,18 @@
 package com.sample;
 
+import com.sample.dao.IAccountDao;
 import com.sample.dao.IRoleDao;
 import com.sample.domain.Role;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.IRObject;
+
+import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -12,6 +21,23 @@ import java.util.*;
  **/
 public class RoleTest {
     private IRoleDao roleDao;
+    private InputStream in;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+
+    @Before
+    public void setUp() throws Exception {
+// 1.读取配置文件
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+// 2.创建构建者对象
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+// 3.创建SqlSession工厂对象
+        factory = builder.build(in);
+// 4.创建SqlSession对象
+        session = factory.openSession();
+        // 5.创建Dao的代理对象
+        roleDao = session.getMapper(IRoleDao.class);
+    }
     @Test
     public void testFindAll() {
         List<Role> roles = roleDao.findAll();
